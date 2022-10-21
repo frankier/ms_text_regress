4d092632b97b256d9ede1b1314d65da64306aa29
Frankie Robertson <frankie@robertson.name>
2022-10-21 20:33:09 +0300

    Add smoke test for BertForOrdinalRegression

---
+++ tests/test_models.py
@@ -0,0 +1,52 @@
+"""
+This module tests the models, but note that they are really more like smoke
+tests, just to test that training and evaluation can run on a single example.
+"""
+from bert_ordinal import BertForOrdinalRegression
+from datasets import load_dataset
+from transformers import Trainer, TrainingArguments
+import os
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'fixtures',
+)
+
+
+def tokenize_dataset(dataset):
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+    def tokenize(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+    return dataset.map(tokenize, batched=True)
+
+
+def mk_toy_trainer(tmp_path, model, dataset):
+    return Trainer(
+        model=model,
+        args=TrainingArguments(
+            output_dir=tmp_path,
+            logging_strategy="no",
+            learning_rate=1e-4,
+            lr_scheduler_type="linear",
+            warmup_ratio=0.5,
+            num_train_epochs=1,
+            optim="adamw_torch"
+        ),
+        train_dataset=dataset,
+        eval_dataset=dataset
+    )
+
+
+def test_bert_for_ordinal_regression(tmp_path):
+    dataset = tokenize_dataset(load_dataset("json", data_files=os.path.join(FIXTURE_DIR, "tiny_shoe_reviews.json"))["train"])
+    num_labels = 5
+
+    model = BertForOrdinalRegression.from_pretrained(
+        "bert-base-cased",
+        num_labels=num_labels
+    )
+    trained = mk_toy_trainer(tmp_path, model, dataset).train()
+    assert trained.global_step == 1

f315cf17974d5f5949f1880b09fdc40a6419bef9
Frankie Robertson <frankie@robertson.name>
2022-10-21 21:58:47 +0300

    Add Sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'bert_ordinal'
+copyright = '2022, Frankie Robertson'
+author = 'Frankie Robertson'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser', 'sphinx.ext.autosummary']
+source_suffix = ['.rst', '.md']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autosummary_generate = True
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,39 @@
+.. bert_ordinal documentation master file, created by
+   sphinx-quickstart on Fri Oct 21 21:08:30 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to bert_ordinal's documentation!
+========================================
+
+bert_ordinal is a Python package for ordinal regression using BERT. The main
+content of the package BERT compatible models based on `transformers`, ready to
+fine-tune on ordinal regression tasks as well as generally applicable functions
+for working with ordinal data. Apart from this, functions for evaluating the
+task and utilities for fetching and processing  some open datasets for ordinal
+regression are provided.
+
+Modules
+-------
+
+.. currentmodule:: bert_ordinal
+
+.. autosummary::
+   :toctree: _autosummary
+   :recursive:
+
+   ordinal_models.bert
+   datasets
+   ordinal
+   eval
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`

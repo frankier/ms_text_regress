3c3146678067f9098340d68cff9258b08a786ef4
Frankie Robertson <frankie@robertson.name>
2022-11-07 10:38:36 +0200

    Update dataset processing for new rt_critics

--- bert_ordinal/datasets.py
+++ bert_ordinal/datasets.py
@@ -3,13 +3,13 @@ This module contains functionality to load and preprocess open data ordinal
 regression datasets.
 """
 
-from typing import List, Tuple, Union
+from typing import List, Optional, Tuple, Union
 
 import datasets
 
 
-def dec_label(example):
-    return {"label": example["label"] - 1}
+def dec_label(row):
+    return {"label": row["label"] - 1}
 
 
 def get_dataset_scale_points_cdr(dataset) -> List[int]:
@@ -19,7 +19,7 @@ def get_dataset_scale_points_cdr(dataset) -> List[int]:
     def process_row(row):
         dataset_scale_points[row["dataset"]] = row["scale_points"]
 
-    dataset.map(process_row)
+    dataset.map(process_row, desc="Getting scale points")
     return dataset_scale_points
 
 
@@ -33,11 +33,13 @@ def get_dataset_scale_points_rt(dataset) -> List[int]:
             )
         dataset_scale_points[row["task_ids"]] = row["scale_points"]
 
-    dataset.map(process_row)
+    dataset.map(process_row, desc="Getting scale points")
     return dataset_scale_points
 
 
-def load_data(name: str) -> Tuple[datasets.DatasetDict, Union[int, List[int]], bool]:
+def load_data(
+    name: str, num_dataset_proc: Optional[int] = None
+) -> Tuple[datasets.DatasetDict, Union[int, List[int]], bool]:
     """
     Loads either the single part/single task dataset "shoe_reviews" or the multi
     part/multi task "cross_domain_reviews" dataset.
@@ -55,21 +57,23 @@ def load_data(name: str) -> Tuple[datasets.DatasetDict, Union[int, List[int]], b
         d = datasets.load_dataset("frankier/cross_domain_reviews")
         assert isinstance(d, datasets.DatasetDict)
         dataset = d
-        dataset = dataset.rename_column("rating", "label")
-        dataset = dataset.map(dec_label)
+        dataset = dataset.rename_columns({"rating": "label", "dataset": "task_ids"})
+        dataset = dataset.map(
+            dec_label, num_proc=num_dataset_proc, desc="Decrementing labels"
+        )
         num_labels = get_dataset_scale_points_cdr(dataset)
-        dataset = dataset.rename_column("dataset", "task_ids")
         is_multi = True
     elif name == "multiscale_rt_critics":
         d = datasets.load_dataset("frankier/processed_multiscale_rt_critics")
         assert isinstance(d, datasets.DatasetDict)
         # DatasetDict({k: v for k, v in d.items() if k in ("train", "test")})
         dataset = d
-        dataset = dataset.rename_column("num", "label")
-        dataset = dataset.rename_column("denom", "scale_points")
-        dataset = dataset.map(dec_label)
-        dataset = dataset.rename_column("group_id", "task_ids")
-        dataset = dataset.rename_column("review_content", "text")
+        dataset = dataset.rename_columns(
+            {
+                "group_id": "task_ids",
+                "review_content": "text",
+            }
+        )
         num_labels = get_dataset_scale_points_rt(dataset)
         is_multi = True
     else:

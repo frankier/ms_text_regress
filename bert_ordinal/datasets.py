7837663199c2f0f84d3ba6034f83d237094beb77
Frankie Robertson <frankie@robertson.name>
2022-12-09 12:18:13 +0200

    Add a couple of RT critics dataset variation

--- bert_ordinal/datasets.py
+++ bert_ordinal/datasets.py
@@ -210,6 +210,25 @@ def sample_multiscale_rt_critics(batch):
     return pyarrow.Table.from_pandas(df)
 
 
+def sample_rt_critics_by_critic_500pl(batch):
+    df = pandas.DataFrame.from_dict(batch.data)
+    df["orig_group_id"] = df["group_id"]
+    # Drop groups with no critic_name given
+    df = df[df["critic_name"].notna()]
+    groups = df.groupby(["critic_name", "group_id"])
+    df["group_id"] = groups.ngroup().astype(int)
+    df = groups.filter(lambda x: len(x) >= 500)
+    return pyarrow.Table.from_pandas(df.sample(frac=1, random_state=42))
+
+
+def load_joined_rt_critics() -> datasets.Dataset:
+    d = datasets.load_dataset("frankier/processed_multiscale_rt_critics")
+    assert isinstance(d, datasets.DatasetDict)
+    d = datasets.concatenate_datasets(list(d.values()))
+    assert isinstance(d, datasets.Dataset)
+    return d
+
+
 def load_data(
     name: str, num_dataset_proc: Optional[int] = None
 ) -> Tuple[datasets.DatasetDict, Union[int, List[int]], bool]:
@@ -237,9 +256,7 @@ def load_data(
         num_labels = get_dataset_scale_points_cdr(dataset)
         is_multi = True
     elif name == "rt_critics_one":
-        d = datasets.load_dataset("frankier/processed_multiscale_rt_critics")
-        assert isinstance(d, datasets.DatasetDict)
-        d = datasets.concatenate_datasets(list(d.values()))
+        d = load_joined_rt_critics()
         df = pandas.DataFrame(d)
         biggest_group_size = 0
         biggest_group_df = None
@@ -259,11 +276,65 @@ def load_data(
         )
         dataset = dataset.rename_columns({"review_content": "text"})
         is_multi = False
+    elif name == "rt_critics_by_critic_500pl":
+        d = load_joined_rt_critics()
+        d = d.map(sample_rt_critics_by_critic_500pl, batched=True, batch_size=None)
+        df = pandas.DataFrame(d)
+        train_df, test_df = stratified_split(df, ["group_id"], random_state=42)
+        dataset = datasets.DatasetDict(
+            {
+                "train": datasets.Dataset.from_pandas(train_df, preserve_index=False),
+                "test": datasets.Dataset.from_pandas(test_df, preserve_index=False),
+            }
+        )
+        dataset = dataset.rename_columns(
+            {
+                "group_id": "task_ids",
+                "review_content": "text",
+            }
+        )
+        num_labels = get_dataset_scale_points_rt(dataset)
+        is_multi = True
+    elif name == "rt_critics_big_irregular_5":
+        d = load_joined_rt_critics()
+        df = pandas.DataFrame(d)
+        critic_publishers_to_select = [
+            # Very few As. Prefers whole letter grades.
+            ("Frank Swietek", "One Guy's Opinion"),
+            ("Mark Dujsik", "Mark Reviews Movies"),
+            ("Emanuel Levy", "EmanuelLevy.Com"),
+            ("Ken Hanke", "Mountain Xpress (Asheville, NC)"),
+            ("Chris Hewitt", "St. Paul Pioneer Press"),
+        ]
+        selected = []
+        for critic_name, publisher in critic_publishers_to_select:
+            sub_df = df[
+                (df["critic_name"] == critic_name) & (df["publisher_name"] == publisher)
+            ]
+            print("Selected", len(sub_df), "reviews from", critic_name, "at", publisher)
+            assert len(sub_df) > 0
+            selected.append(sub_df)
+        df = pandas.concat(selected)
+        groups = df.groupby(["critic_name", "group_id"])
+        df["orig_group_id"] = df["group_id"]
+        df["group_id"] = groups.ngroup().astype(int)
+        train_df, test_df = stratified_split(df, ["group_id"], random_state=42)
+        dataset = datasets.DatasetDict(
+            {
+                "train": datasets.Dataset.from_pandas(train_df, preserve_index=False),
+                "test": datasets.Dataset.from_pandas(test_df, preserve_index=False),
+            }
+        )
+        dataset = dataset.rename_columns(
+            {
+                "group_id": "task_ids",
+                "review_content": "text",
+            }
+        )
+        num_labels = get_dataset_scale_points_rt(dataset)
+        is_multi = True
     elif name == "multiscale_rt_critics":
-        d = datasets.load_dataset("frankier/processed_multiscale_rt_critics")
-        assert isinstance(d, datasets.DatasetDict)
-        d = datasets.concatenate_datasets(list(d.values()))
-        assert isinstance(d, datasets.Dataset)
+        d = load_joined_rt_critics()
         d = d.map(sample_multiscale_rt_critics, batched=True, batch_size=None)
         """
         df = pandas.DataFrame(d["train"])

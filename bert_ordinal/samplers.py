1937dd56fa4a3d2dcbe438d3004d5b3b2a2a149c
Frankie Robertson <frankie@robertson.name>
2022-12-27 10:30:13 +0200

    Add some samplers

---
+++ bert_ordinal/samplers.py
@@ -0,0 +1,177 @@
+import numpy as np
+from torch.utils.data import Sampler
+
+from bert_ordinal.vendor.hierarchical_sampler import safe_random_choice
+
+
+def ensure_full_batches(instances, batch_size):
+    remainders = len(instances) % batch_size
+    if remainders == 0:
+        return instances
+    # Oversample just enough to make constant sized batches
+    return np.concatenate(
+        (instances, safe_random_choice(instances, batch_size - remainders))
+    )
+
+
+class TaskAtATimeSampler(Sampler):
+    """
+    This sampler puts a single task in each batch. In case the samplers per task
+    does not divide the batch size, it slightly oversamples.
+    """
+
+    def __init__(
+        self,
+        task_ids,
+        batch_size,
+    ):
+        self.task_ids = task_ids
+        self.batch_size = batch_size
+        self.reshuffle()
+
+    def reshuffle(self):
+        self.batches = []
+        for task_id in self.task_ids:
+            instances = ensure_full_batches(
+                np.where(self.task_ids == task_id)[0], self.batch_size
+            )
+            np.random.shuffle(instances)
+            for i in range(0, len(instances), self.batch_size):
+                self.batches.append(instances[i : i + self.batch_size])
+        np.random.shuffle(self.batches)
+
+    def __iter__(self):
+        self.reshuffle()
+        for batch in self.batches:
+            yield [int(x) for x in batch]
+
+    def __len__(self):
+        return len(self.batches)
+
+
+class LabelStratifiedTaskAtATimeSampler(Sampler):
+    """
+    This sampler is similar to TaskAtATimeSampler, but it ensures that each
+    batch contains a stratified sample of the labels (without any regard
+    considering them as ordinal).
+    """
+
+    def __init__(
+        self,
+        task_ids,
+        labels,
+        batch_size,
+    ):
+        self.task_ids = task_ids
+        self.labels = labels
+        self.batch_size = batch_size
+        self.reshuffle()
+
+    def reshuffle(self):
+        self.batches = []
+        for task_id in self.task_ids:
+            instances = ensure_full_batches(
+                np.nonzero(self.task_ids == task_id)[0], self.batch_size
+            )
+            np.random.shuffle(instances)
+            # Use a stable sort to preserve the shuffle order
+            instances = instances[np.argsort(self.labels[instances], kind="stable")]
+            num_batches = len(instances) // self.batch_size
+            for i in range(num_batches):
+                self.batches.append(instances[i::num_batches])
+        np.random.shuffle(self.batches)
+
+    def __iter__(self):
+        self.reshuffle()
+        for batch in self.batches:
+            yield [int(x) for x in batch]
+
+    def __len__(self):
+        return len(self.batches)
+
+
+class FixedTaskLabelSampler(Sampler):
+    """
+    This sampler has a fixed number of tasks and labels per batch.
+    """
+
+    def __init__(
+        self,
+        task_ids,
+        labels,
+        tasks_per_batch,
+        labels_per_task,
+        batch_size,
+    ):
+        self.task_ids = np.array(task_ids)
+        self.labels = np.array(labels)
+        self.batch_size = batch_size
+        self.tasks_per_batch = tasks_per_batch
+        self.labels_per_task = labels_per_task
+        self.reshuffle()
+
+    def reshuffle(self):
+        self.batches = []
+        by_task_label = {}
+        tasks_left = np.empty(self.task_ids.max() + 1, dtype=int)
+        labels_left = {}
+        label_sorter = np.argsort(self.labels)
+        task_sorter = np.argsort(self.task_ids[label_sorter], kind="stable")
+        sorter = label_sorter[task_sorter]
+        unsorter = np.argsort(sorter)
+        labels_sorted = self.labels[sorter]
+        for task_id, start, cnt in zip(
+            *np.unique(self.task_ids[sorter], return_index=True, return_counts=True)
+        ):
+            task_labels = labels_sorted[start : start + cnt]
+            tasks_left[task_id] = cnt
+            labels_left[task_id] = np.zeros(task_labels.max() + 1, dtype=np.int)
+            for label, label_start, label_cnt in zip(
+                *np.unique(task_labels, return_index=True, return_counts=True)
+            ):
+                full_start = start + label_start
+                by_task_label.setdefault(task_id, {})[label] = unsorter[
+                    full_start : full_start + label_cnt
+                ]
+                labels_left[task_id][label] = label_cnt
+        subbatch_size = self.tasks_per_batch * self.labels_per_task
+        while 1:
+            available_tasks = np.nonzero(tasks_left > 0)[0]
+            if len(available_tasks) == 0:
+                break
+            # TODO: Rather than using safe_random_choice, it should fall back to using non-available tasks
+            chosen_tasks = safe_random_choice(available_tasks, self.tasks_per_batch)
+            for task_id in chosen_tasks:
+                # TODO: Rather than using safe_random_choice, it should fall back to using non-available labels
+                available_labels = np.nonzero(labels_left[task_id] > 0)[0]
+                if len(available_labels) == 0:
+                    available_labels = np.array(list(by_task_label[task_id].keys()))
+                chosen_labels = safe_random_choice(
+                    available_labels, self.labels_per_task
+                )
+                for label in chosen_labels:
+                    idx = (
+                        len(by_task_label[task_id][label]) - labels_left[task_id][label]
+                    )
+                    full_batch = by_task_label[task_id][label]
+                    batch = full_batch[idx : idx + subbatch_size]
+                    tasks_left[task_id] -= len(batch)
+                    labels_left[task_id][label] -= len(batch)
+                    if len(batch) < subbatch_size:
+                        batch = np.concatenate(
+                            (
+                                batch,
+                                safe_random_choice(
+                                    full_batch, subbatch_size - len(batch)
+                                ),
+                            )
+                        )
+                    self.batches.append(batch)
+
+    def __iter__(self):
+        self.reshuffle()
+        for batch in self.batches:
+            yield [int(x) for x in batch]
+
+    def __len__(self):
+        return len(self.batches)
---
+++ bert_ordinal/vendor/hierarchical_sampler.py
@@ -0,0 +1,128 @@
+"""
+Taken from pytorch-metric-learning and modified to output lists of ints rather than numpy arrays.
+
+Originally Apache-v2 licensed.
+
+https://github.com/KevinMusgrave/pytorch-metric-learning/blob/345cb2b0cabfd39cc7a25ff2a81fa29adc3ea885/src/pytorch_metric_learning/samplers/hierarchical_sampler.py
+"""
+import itertools
+from collections import defaultdict
+
+import numpy as np
+import torch
+from torch.utils.data.sampler import BatchSampler
+
+
+def safe_random_choice(input_data, size):
+    """
+    Randomly samples without replacement from a sequence. It is "safe" because
+    if len(input_data) < size, it will randomly sample WITH replacement
+    Args:
+        input_data is a sequence, like a torch tensor, numpy array,
+                        python list, tuple etc
+        size is the number of elements to randomly sample from input_data
+    Returns:
+        An array of size "size", randomly sampled from input_data
+    """
+    replace = len(input_data) < size
+    return np.random.choice(input_data, size=size, replace=replace)
+
+
+# Inspired by
+# https://github.com/kunhe/Deep-Metric-Learning-Baselines/blob/master/datasets.py
+class HierarchicalSampler(BatchSampler):
+    def __init__(
+        self,
+        labels,
+        batch_size,
+        samples_per_class,
+        batches_per_super_tuple=4,
+        super_classes_per_batch=2,
+        inner_label=0,
+        outer_label=1,
+    ):
+        """
+        labels: 2D array, where rows correspond to elements, and columns correspond to the hierarchical labels
+        batch_size: because this is a BatchSampler the batch size must be specified
+        samples_per_class: number of instances to sample for a specific class. set to "all" if all element in a class
+        batches_per_super_tuples: number of batches to create for a pair of categories (or super labels)
+        inner_label: columns index corresponding to classes
+        outer_label: columns index corresponding to the level of hierarchy for the pairs
+        """
+        if torch.is_tensor(labels):
+            labels = labels.cpu().numpy()
+
+        self.batch_size = batch_size
+        self.batches_per_super_tuple = batches_per_super_tuple
+        self.samples_per_class = samples_per_class
+        self.super_classes_per_batch = super_classes_per_batch
+
+        # checks
+        assert (
+            self.batch_size % super_classes_per_batch == 0
+        ), f"batch_size should be a multiple of {super_classes_per_batch}"
+        self.sub_batch_len = self.batch_size // super_classes_per_batch
+
+        if self.samples_per_class != "all":
+            assert self.samples_per_class > 0
+            assert (
+                self.sub_batch_len % self.samples_per_class == 0
+            ), "batch_size not a multiple of samples_per_class"
+
+        all_super_labels = set(labels[:, outer_label])
+        self.super_image_lists = {slb: defaultdict(list) for slb in all_super_labels}
+        for idx, instance in enumerate(labels):
+            slb, lb = instance[outer_label], instance[inner_label]
+            self.super_image_lists[slb][lb].append(idx)
+
+        self.super_pairs = list(
+            itertools.combinations(all_super_labels, super_classes_per_batch)
+        )
+        self.reshuffle()
+
+    def __iter__(
+        self,
+    ):
+        self.reshuffle()
+        for batch in self.batches:
+            yield [int(x) for x in batch]
+
+    def __len__(
+        self,
+    ):
+        return len(self.batches)
+
+    def reshuffle(self):
+        batches = []
+        for combinations in self.super_pairs:
+
+            for b in range(self.batches_per_super_tuple):
+
+                batch = []
+                for slb in combinations:
+
+                    sub_batch = []
+                    all_classes = list(self.super_image_lists[slb].keys())
+                    np.random.shuffle(all_classes)
+                    for cl in all_classes:
+                        if len(sub_batch) >= self.sub_batch_len:
+                            break
+                        instances = self.super_image_lists[slb][cl]
+                        samples_per_class = (
+                            self.samples_per_class
+                            if self.samples_per_class != "all"
+                            else len(instances)
+                        )
+                        if len(sub_batch) + samples_per_class > self.sub_batch_len:
+                            continue
+                        sub_batch.extend(
+                            safe_random_choice(instances, size=samples_per_class)
+                        )
+
+                    batch.extend(sub_batch)
+
+                np.random.shuffle(batch)
+                batches.append(batch)
+
+        np.random.shuffle(batches)
+        self.batches = batches

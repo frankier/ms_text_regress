ba63f882351d650e80dd936ac14333b14e690add
Frankie Robertson <frankie@robertson.name>
2023-10-03 14:42:27 +0300

    Don't use nightly pytorc

--- install_dev_cpu.sh
+++ install_dev_cpu.sh
@@ -1,4 +1,3 @@
 poetry install --all-extras --with=dev && \
-	poetry run pip install --force-reinstall --pre \
-	--extra-index-url https://download.pytorch.org/whl/nightly/cpu \
+	poetry run pip install --force-reinstall \
 	torch

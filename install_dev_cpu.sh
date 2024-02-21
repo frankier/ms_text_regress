5a12ae91b9fb876b28d1aebc9b090d108472abdd
Frankie Robertson <frankie@robertson.name>
2024-02-21 16:52:37 +0200

    Update install script

--- install_dev_cpu.sh
+++ install_dev_cpu.sh
@@ -1,3 +1,10 @@
-poetry install --all-extras --with=dev && \
-	poetry run pip install --force-reinstall \
-	transformers[torch] torch --index-url https://download.pytorch.org/whl/cpu
+poetry export --without-hashes -E demos -E datasets -E eval -E viewer --with=dev -o tmp-requirements.txt
+
+sed -i '/^torch\b/d' tmp-requirements.txt
+echo 'torch --extra-index-url https://download.pytorch.org/whl/cpu' >> tmp-requirements.txt
+
+sed -i '/^accelerate\b/d' tmp-requirements.txt
+echo '-e git+https://github.com/frankier/accelerate.git@73ccf16c3cf1c9c3d0c196d11d9c5b14a0520758#egg=accelerate' >> tmp-requirements.txt
+
+poetry run pip install -r tmp-requirements.txt
+rm tmp-requirements.txt
